ae19ec57a0f88e7f616e9d4ecbc3bd69c73425e5
devinvb <devin_@live.nl>
2017-01-13 21:44:50 +0100

    registration menu

--- main/static/js/myjs.js
+++ main/static/js/myjs.js
@@ -14,7 +14,17 @@ $(function() {
         //disable link click
         e.preventDefault();
         //show menu
+        $("#regmenu").removeClass("active");
         $("#loginmenu").toggleClass("active");
+    });
+
+    //register menu
+    $("#reglink").click( function(e) {
+        //disable link click
+        e.preventDefault();
+        //show menu
+        $("#loginmenu").removeClass("active");
+        $("#regmenu").toggleClass("active");
     });
 
 
