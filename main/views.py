e6c366b5887939d9bc0b0dcd1b1b438d9c681901
akbokha <a.k.bokharouss@student.tue.nl>
2017-01-15 17:25:40 +0100

    Fix for search queries majors

--- main/views.py
+++ main/views.py
@@ -141,10 +141,12 @@ def search(request):
         form = UserForm()
 
     query = request.GET['q']
+    major = query.replace(" ", "")
+    major = major.lower()
     results = Resource.objects.filter(title__icontains=query)
-    courses = Course.objects.filter(name__icontains=query) | Course.objects.filter(major__icontains=query)
-    summaries = Resource.objects.filter(title__icontains=query).filter(resourcetype = "Summary") | Resource.objects.filter(major__icontains=query)
-    exams = Resource.objects.filter(title__icontains=query).filter(resourcetype = "Exam") | Resource.objects.filter(major__icontains=query)
+    courses = Course.objects.filter(name__icontains=query) | Course.objects.filter(major__icontains=major)
+    summaries = Resource.objects.filter(title__icontains=query).filter(resourcetype = "Summary") | Resource.objects.filter(major__icontains=query).filter(resourcetype = "Summary")
+    exams = Resource.objects.filter(title__icontains=query).filter(resourcetype = "Exam") | Resource.objects.filter(major__icontains=query).filter(resourcetype = "Exam")
     temp = get_template('main/searchresults.html')
     context = Context({'results': results, 'query': query})
     return render(request, 'main/searchresults.html', {'courses': courses, 'summaries': summaries, 'results': results, 'exams': exams, 'query': query} )
